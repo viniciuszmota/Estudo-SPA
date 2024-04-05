c477197fba51048eb1d46a9fbb0a514b18645b5b
Vinicius Mota <vinicius.quimica@gmail.com>
2024-04-05 14:29:45 -0300

    spa

---
+++ js/index.js
@@ -0,0 +1,12 @@
+import { Router } from './router.js'
+
+const router = new Router()
+router.add("/", "/pages/home.html")
+router.add("/about", "/pages/about.html")
+router.add("/contact", "/pages/contact.html")
+router.add("/error", "/pages/error.html")
+
+router.handle()
+
+window.onpopstate = () => router.handle()
+window.route = () => router.route()
